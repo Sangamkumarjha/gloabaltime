9ddf9b4f9226534a477296f2dcc4753b212fbc2a
Sangamkumarjha <jhakumarsangam@gmail.com>
2025-06-13 13:05:42 +0530

    Fix: 2nd use

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import DownloadPlan from "./pages/DownloadPlan";
